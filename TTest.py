9adeb4d587074222b2ce35e1a864b8cd93e26f8d
Mike Spaans <99069233@mydavinci.nl>
2022-02-17 09:34:48 +0100

    Yahtzee bezig met maken functies van combinaties

--- TTest.py
+++ TTest.py
@@ -1,15 +1,4 @@
-list1 = [1, 2, 4]
-list2 = [4, 5, 6]
-
-set_difference = set(list1) - set(list2)
-list_difference = list(set_difference)
-
-print(list_difference)
-
-def DobbelSteenOpnieuwGooien(VerwijderDobbelSteenList, DobbelStenenList):
-    VerwijderdeDobbel = VerwijderDobbelSteenList.remove(1)
-    print (VerwijderDobbelSteenList)
-    DobbelStenenList.remove(VerwijderdeDobbel)
-    DobbelSteen = random.randint(1,6)
-    DobbelStenenList.append(DobbelSteen)
-    return DobbelStenenList
+a = {1,2,3,3}
+b = {3,3,4,5}
+c = a.intersection(b)
+print(c) # {3}
--- Yahtzee.py
+++ Yahtzee.py
@@ -1,12 +1,18 @@
 import random
-def SorrySnapIkNiet():
-    print ("Sorry dat snap ik niet")
+def VakjeAlIngevuld():
+    print ("Dit vakje heb je al inveguld. ")
+
+Hoeveel_Eenen = 0
+Hoeveel_Tweeën = 0
+Hoeveel_Drieën = 0
+Hoeveel_Vieren = 0
+Hoeveel_Vijfen = 0
+Hoeveel_Zessen = 0
 
 
 GegooideDobbelStenenList = []
 
 
-    
 # 5 dobbelstenen dobbelen
 def DobbelSteenGooienToevoegen(DobbelStenenList ,HoeveelGooien):
     for i in range (HoeveelGooien):
@@ -14,46 +20,132 @@ def DobbelSteenGooienToevoegen(DobbelStenenList ,HoeveelGooien):
         DobbelStenenList.append(DobbelSteen)
     print (DobbelStenenList)
     return DobbelStenenList
-# print (DobbelSteenGooienToevoegen(5))
-
 
     
 # vragen welke de speler opnieuw wilt dobbelen
 def WilOpnieuwDobbelen(DobbelStenenList):
-    WilOpnieuw = input("Wilt u nog een keer gooien? ")
-    if WilOpnieuw == "nee" or WilOpnieuw == "N":
-        pass
-    elif WilOpnieuw == "ja" or WilOpnieuw == "J":
+    BeurtOpnieuw = 0
+    while BeurtOpnieuw < 3:
+        HoeveelToevoegen = 0
         DoorVragen = 0
         OpnieuwVragen = True
-        while DoorVragen <= 4 and OpnieuwVragen == True:
-            WelkeOpnieuwGooien = input("Welk nummer wilt u opnieuw gooien? (typ stop als je niet meer opnieuw wilt gooien) ")
-            if WelkeOpnieuwGooien == "stop":
-                OpnieuwVragen = False
-            GetalofNummer = WelkeOpnieuwGooien.isdigit()
-            if GetalofNummer == True:
-                DobbelStenenList.remove(int(WelkeOpnieuwGooien))
+        WilOpnieuw = input("Wil je nog een keer gooien? ")
+        if WilOpnieuw == "nee" or WilOpnieuw == "N":
+            return DobbelStenenList
+        elif WilOpnieuw == "ja" or WilOpnieuw == "J":
+            while DoorVragen <= 4 and OpnieuwVragen == True:
+                WelkeOpnieuwGooien = input("Welk nummer wil je opnieuw gooien? (typ stop als je niet meer opnieuw wilt gooien) ")
+                if WelkeOpnieuwGooien == "stop":
+                    OpnieuwVragen = False
+                GetalofNummer = WelkeOpnieuwGooien.isdigit()
+                if GetalofNummer == True:
+                    DobbelStenenList.remove(int(WelkeOpnieuwGooien))
+                    HoeveelToevoegen += 1
+                DoorVragen += 1
+            for i in range (HoeveelToevoegen):
                 DobbelSteenDobbelen = random.randint(1, 6)
                 DobbelStenenList.append(DobbelSteenDobbelen)
-            DoorVragen += 1
-        return DobbelStenenList
-
-    else:
-        SorrySnapIkNiet()
-
-GegooideDobbelStenen = DobbelSteenGooienToevoegen(GegooideDobbelStenenList, 5)
-OngewensteDobbelStenen = WilOpnieuwDobbelen(GegooideDobbelStenenList)
-print (OngewensteDobbelStenen)
-
-
-
-
-
+            print (DobbelStenenList)
+        BeurtOpnieuw += 1
+    return DobbelStenenList
 
 # Three of a kind: drie dobbelstenen met dezelfde punten
+# laatste deel werkt nog niet
+def ThreeOfAKind(DobbelList):
+    DobbelListSet = set(DobbelList)
+    DobbelListDict = {"1" : 0, "2" : 0, "3" : 0, "4" : 0, "5" : 0, "6" : 0}
+    if len(DobbelListSet) <= 3:
+        for i in (DobbelList):
+            DobbelListDict[str(i)] += 1
+        # for i in range (6):
+        if int('1') in DobbelListDict > 2:
+            print ("Je hebt Three of a Kind")
+        # print (DobbelListDict)
+        
+
 # Four of a  kind: vier dobbelstenen met dezelfde punten
+def FourOFAKind(DobbelList):
+    pass
 # Full House: 25 punten voor 3 gelijke en een paar (5 gelijke telt niet als full house tenzij het vak Yahtzee reeds is ingevuld.)
+def FullHouse(DobbelList):
+    # set gebruiken
+    pass
 # Small straight 0 punten voor vier oplopende dobbelstenen(volgorde maakt niet uit)
+def SmallStraight(DobbelList):
+    DobbelListSet = set(DobbelList)
+    if len(DobbelListSet) == 4:
+        print ("Je hebt een Small straight. ")
 # Large straight 40 punten voor 5 oplopende dobbelstenen (Volgorde maakt niet uit)
+def LargeStraight(DobbelList):
+    pass
 # Yahtzee (top score) 50 punten 50 punten als alle dobbelsten gelijk zijn
+def Yahtzee(DobbelList):
+    # set gebruiken
+    pass
 # Chance: totaal aantal punten van alle gegooiden dobbelstenen.
+def Chance(DobbelList):
+    pass
+
+
+
+# het scoreboord
+def ScoreBoord(Eenen, Tweeën, Drieën, Vieren, Vijfen, Zessen):
+    OpnieuwBlijvenVragen = True
+    while OpnieuwBlijvenVragen == True:
+        WelkeInvullen = input("Welk vakje van het scoreboord wil je invullen? ")
+        WelkeInvullen = WelkeInvullen.lower()
+        if WelkeInvullen == "aces":
+            if Eenen > 0:
+                VakjeAlIngevuld()
+            else:
+                Eenen = input("Hoeveel Aces heb je? ")
+                OpnieuwBlijvenVragen = False
+        elif WelkeInvullen == "twos":
+            if Tweeën > 0:
+                VakjeAlIngevuld()
+            else:
+                Tweeën = input("Hoeveel Twos heb je? ")
+                OpnieuwBlijvenVragen = False
+        elif WelkeInvullen == "threes":
+            if Drieën > 0:
+                VakjeAlIngevuld()
+            else:
+                Drieën = input("Hoeveel Threes heb je? ")
+                OpnieuwBlijvenVragen = False
+        elif  WelkeInvullen == "fours":
+            if Vieren > 0:
+                VakjeAlIngevuld()
+            else:
+                Vieren = input("Hoeveel Fours heb je? ")
+                OpnieuwBlijvenVragen = False
+        elif WelkeInvullen == "fives":
+            if Vijfen > 0:
+                VakjeAlIngevuld()
+            else:
+                Vijfen = input("Hoeveel Fives heb je? ")
+                OpnieuwBlijvenVragen = False
+        elif WelkeInvullen == "sixes":
+            if Zessen > 0:
+                VakjeAlIngevuld()
+            else:
+                OpnieuwBlijvenVragen = False
+                Zessen = input("Hoeveel Sixes heb je? ")
+        print ("Aces ", Eenen)
+        print ("Twos ", Tweeën)
+        print ("Threes ", Drieën)
+        print ("Fours ", Vieren)
+        print ("Fives ", Vijfen)
+        print ("Sixes ", Zessen)
+
+
+
+GegooideDobbelStenen = DobbelSteenGooienToevoegen(GegooideDobbelStenenList, 5)
+print ("")
+NieuweDobbelen = WilOpnieuwDobbelen(GegooideDobbelStenenList)
+print ("")
+# ThreeOfAKind(NieuweDobbelen)
+SmallStraight(NieuweDobbelen)
+print ("")
+
+ScoreBoord(Hoeveel_Eenen, Hoeveel_Tweeën, Hoeveel_Drieën, Hoeveel_Vieren, Hoeveel_Vijfen, Hoeveel_Zessen)
+
